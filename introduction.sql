103d0b8303cc7736469080a77e1b28beb572785b
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-08-30 06:52:44 -0700

    Create introduction.sql

---
+++ introduction.sql
@@ -0,0 +1,12 @@
+What is MySQL?
+MySQL is a very popular open-source relational database management system (RDBMS).
+
+MySQL is open-source
+MySQL is free
+MySQL is ideal for both small and large applications
+MySQL is very fast, reliable, scalable, and easy to use
+MySQL is cross-platform
+MySQL is compliant with the ANSI SQL standard
+MySQL was first released in 1995
+MySQL is developed, distributed, and supported by Oracle Corporation
+MySQL is named after co-founder Ulf Michael "Monty" Widenius's daughter: My
