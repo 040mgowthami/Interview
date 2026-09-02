893b1c890a36b40dd99553918d980b4540c678a8
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-02 09:42:02 -0700

    Update introduction.sql

--- introduction.sql
+++ introduction.sql
@@ -25,3 +25,12 @@ SQL can create stored procedures in a database
 SQL can create views in a database
 SQL can set permissions on tables, procedures, and views
 
+
+Using SQL in Your Web Site
+To build a web site that shows data from a database, you will need:
+
+An RDBMS database program (i.e. MS Access, SQL Server, MySQL)
+To use a server-side scripting language, like PHP or ASP
+To use SQL to get the data you want
+To use HTML / CSS to style the page
+
