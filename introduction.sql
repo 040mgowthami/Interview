8b7381a37c8f144becd641f503d1bd5e62894347
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-02 09:43:26 -0700

    Update introduction.sql

--- introduction.sql
+++ introduction.sql
@@ -22,6 +22,24 @@ SQL can delete records from a database
 SQL can create new databases
 SQL can create new tables in a database
 SQL can create stored procedures in a database
+
+
+RDBMS
+RDBMS stands for Relational Database Management System.
+
+RDBMS is the basis for SQL, and for all modern database systems such as MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.
+
+The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and it consists of columns and rows.
+
+Look at the "Customers" table:
+
+ExampleGet your own SQL Server
+SELECT * FROM Customers;
+
+
+The columns in the "Customers" table consist of CustomerID, CustomerName, ContactName, Address, City, PostalCode and Country. A column is a vertical entity in a table.
+
+A record, also called a row, is each individual entry that exists in a table. For example, there are 91 records in the above Customers table. A record is a horizontal entity in a table.
 SQL can create views in a database
 SQL can set permissions on tables, procedures, and views
 
