a00425fdf9b8d3a82bcc8db944a4d427d594c337
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-02 09:34:10 -0700

    Update introduction.sql

--- introduction.sql
+++ introduction.sql
@@ -10,3 +10,18 @@ MySQL is compliant with the ANSI SQL standard
 MySQL was first released in 1995
 MySQL is developed, distributed, and supported by Oracle Corporation
 MySQL is named after co-founder Ulf Michael "Monty" Widenius's daughter: My
+
+
+What Can SQL do?
+  
+SQL can execute queries against a database
+SQL can retrieve data from a database
+SQL can insert records in a database
+SQL can update records in a database
+SQL can delete records from a database
+SQL can create new databases
+SQL can create new tables in a database
+SQL can create stored procedures in a database
+SQL can create views in a database
+SQL can set permissions on tables, procedures, and views
+
