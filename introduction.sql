87f705cfe5d0b7d9c62fc6d7038205fd94bf1d33
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-03 10:17:20 -0700

    Update introduction.sql

--- introduction.sql
+++ introduction.sql
@@ -79,6 +79,10 @@ INSERT INTO - inserts new data into a database
 CREATE DATABASE - creates a new database
 ALTER DATABASE - modifies a database
 CREATE TABLE - creates a new table
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index
 
 
 
