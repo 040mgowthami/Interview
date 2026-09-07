bbc7b0403b3c4e361b8aa19cd9c5558619b03096
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-07 09:37:02 -0700

    Update select_cammand.sql

--- select_cammand.sql
+++ select_cammand.sql
@@ -1,6 +1,7 @@
 /* SELECT Syntax */
 SELECT column1, column2, ...
 FROM table_name;
+/*Here, column1, column2, ... are the column names in the table you want to select data from.*/
 
 /* example */
 SELECT CustomerName, City FROM Customers;
