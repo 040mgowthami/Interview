d1d82860b04907aa8b4c4cdbb0f865b8380fa4b1
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-10 09:13:53 -0700

    Update sql_where.sql

--- sql_where.sql
+++ sql_where.sql
@@ -2,3 +2,7 @@
 The WHERE clause is used to filter records.
 
 The WHERE clause is used to extract only those records that fulfill a specific condition. */
+/* ExampleGet your own SQL Server
+Here we select all customers from Mexico:*/
+SELECT * FROM Customers
+WHERE Country = 'Mexico';
