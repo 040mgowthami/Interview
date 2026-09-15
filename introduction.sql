3ba98361931b21b4af5038a1f192598aed08a9ec
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-15 07:52:37 -0700

    Update introduction.sql

--- introduction.sql
+++ introduction.sql
@@ -8,16 +8,12 @@ How to Use SQL
 The following SQL statement selects all the records from the "Customers" table:
 
 
-What Can SQL do?
-  
-SQL can execute queries against a database
-SQL can retrieve data from a database
-SQL can insert records in a database
-SQL can update records in a database
-SQL can delete records from a database
-SQL can create new databases
-SQL can create new tables in a database
-SQL can create stored procedures in a database
+How to Use SQL
+The following SQL statement selects all the records from the "Customers" table:
+
+ExampleGet your own SQL Server
+SELECT * FROM Customers;
+
 
 
 RDBMS
