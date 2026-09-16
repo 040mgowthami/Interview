2ed7a8a7bb11650058f3d897534ad9c3c5a6009e
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-16 09:56:14 -0700

    Correct formatting in Customers table example
    Fixed formatting issues in the Customers table example.

--- introduction.sql
+++ introduction.sql
@@ -96,12 +96,12 @@ The table_name represents the name of the table you want to select data from.
 Demo Database
 Below is a selection from the Customers table used in the examples:
 CustomerID	CustomerName	ContactName	Address	City	PostalCode	Country
-1
+
 
 Alfreds Futterkiste	Maria Anders	Obere Str. 57	Berlin	12209	Germany
 2	Ana Trujillo Emparedados y helados	Ana Trujillo	Avda. de la Constitución 2222	México D.F.	05021	Mexico
 3Antonio Moreno Taquería	Antonio Moreno	Mataderos 2312	México D.F.	05023	Mexico
-4
+
 
 Around the Horn	Thomas Hardy	120 Hanover Sq.	London	WA1 1DP	UK
 5	Berglunds snabbköp	Christina Berglund	Berguvsvägen 8	Luleå	S-958 22	Sweden
