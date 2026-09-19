0fc45e95e1ae90d7ef4032fa8cdfb28c671c35ef
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-19 10:09:27 -0700

    Clarify default sorting order in ORDER BY documentation
    Added clarification about default sorting order.

--- orderby .sql
+++ orderby .sql
@@ -1,4 +1,4 @@
 /*The MySQL ORDER BY Keyword
 The ORDER BY keyword is used to sort the result-set in ascending or descending order.*/
-
+The ORDER BY keyword sorts the records in ascending order (ASC) by default.
 
