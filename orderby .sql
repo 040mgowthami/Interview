d4d45bb9cc490271dc6c8443dd1bdbfaec5afcf5
Gowthami M <168198692+040mgowthami@users.noreply.github.com>
2026-09-19 10:10:11 -0700

    Update orderby .sql

--- orderby .sql
+++ orderby .sql
@@ -1,4 +1,8 @@
 /*The MySQL ORDER BY Keyword
 The ORDER BY keyword is used to sort the result-set in ascending or descending order.*/
 The ORDER BY keyword sorts the records in ascending order (ASC) by default.
+  Example
+
+SELECT * FROM Products
+ORDER BY Price;
 
